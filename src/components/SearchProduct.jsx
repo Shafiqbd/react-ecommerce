0093d61d1eece802c861d809b0818ef694202dc8
Md. Mehedi Hasan <47135970+MdMehedi608@users.noreply.github.com>
2022-03-01 23:36:46 +0600

    search working

--- src/components/SearchProduct.jsx
+++ src/components/SearchProduct.jsx
@@ -1,11 +1,5 @@
-import { TextField } from "@mui/material";
-
 const SearchProduct = () => {
-  return (
-    <div>
-      <TextField id="outlined-basic" label="Search" variant="outlined" fullWidth />
-    </div>
-  );
+  return <div>{/* <TextField id="outlined-basic" label="Search" variant="outlined" fullWidth /> */}</div>;
 };
 
 export default SearchProduct;
