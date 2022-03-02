167959e7d0226e0f68972b7ea41cc74e771e2b16
Md. Mehedi Hasan <47135970+MdMehedi608@users.noreply.github.com>
2022-03-02 23:07:26 +0600

    text filed working

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,5 +1,20 @@
+import { TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
 const AddProduct = () => {
-  return (  );
-}
- 
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        "& > :not(style)": { m: 1 },
+      }}
+    >
+      <TextField helperText="Please enter your name" id="demo-helper-text-aligned" label="Name" />
+      <TextField helperText=" " id="demo-helper-text-aligned-no-helper" label="Name" />
+    </Box>
+  );
+};
+
 export default AddProduct;
