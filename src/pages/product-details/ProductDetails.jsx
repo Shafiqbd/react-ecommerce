aad8666592f80af282967e1604e0536417b48fb3
Md. Mehedi Hasan <47135970+MdMehedi608@users.noreply.github.com>
2022-03-02 22:38:47 +0600

    details done

--- src/pages/product-details/ProductDetails.jsx
+++ src/pages/product-details/ProductDetails.jsx
@@ -1,6 +1,7 @@
-import { Button, Card, Grid } from "@mui/material";
+import { Button, Card, Grid, Rating } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
+import ButtonStyle from "../../components/ButtonStyle";
 
 const Title = styled.h2`
   margin: 0;
@@ -8,34 +9,49 @@ const Title = styled.h2`
 `;
 const ProductDetailsWrap = styled.div`
   & h1 {
-    font-size: 25px;
-    font-weight: 700;
-    margin-top: 50px;
+    font-size: 30px;
+    font-weight: normal;
   }
   & h4 {
-    color: green;
-    font-size: 20px;
+    color: #000;
+    font-size: 24px;
     font-weight: 500;
+    padding-top: 10px;
   }
   & h3 {
     color: gray;
     font-size: 20px;
     font-weight: 500;
+    margin-top: 20px;
+    text-transform: uppercase;
   }
-  & img {
-    height: 400px;
-    width: 100%;
-  }
+
   & p {
     line-height: 30px;
     text-align: justify;
   }
+`;
+const ImgContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 0;
+  height: 400px;
+  & img {
+    // height: auto;
+    width: 100%;
+  }
+`;
+const RatingContent = styled(Rating)`
+  color: #000;
+  font-size: 18px;
 `;
 const TitleWrap = styled.div`
   display: flex;
   align-items: center;
   margin: 30px 0px;
 `;
+const ButtonContent = styled.div``;
 const ProductDetails = ({ productDetails, setProductDetails }) => {
   const back = () => {
     setProductDetails(null);
@@ -53,13 +69,20 @@ const ProductDetails = ({ productDetails, setProductDetails }) => {
         {productDetails ? (
           <Grid container spacing={4}>
             <Grid item md={6}>
-              <img src={productDetails.image} alt="" />
+              <ImgContent>
+                <img src={productDetails.image} alt="" />
+              </ImgContent>
             </Grid>
             <Grid item md={6}>
+              <h3> {productDetails.category}</h3>
               <h1>{productDetails.title}</h1>
-              <h3> Category: {productDetails.category}</h3>
-              <h4> Price: {productDetails.price} BDT.</h4>
+              <RatingContent name="read-only" value={productDetails?.rating?.rate} readOnly />
+              <h4> {productDetails.price} BDT.</h4>
               <p>{productDetails.description}</p>
+              <ButtonContent>
+                <ButtonStyle btnName={"Add to Cart"} width={"140px"} margin_left={"0px"} back={""} />
+                <ButtonStyle btnName={"Go to Cart"} width={"140px"} margin_left={"15px"} back={"bg"} />
+              </ButtonContent>
             </Grid>
           </Grid>
         ) : (
