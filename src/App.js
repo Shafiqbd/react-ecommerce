5c53a57a6f7fed2e9d19b5b9c8c500aea30ed829
Md. Mehedi Hasan <47135970+MdMehedi608@users.noreply.github.com>
2022-03-29 11:54:29 +0600

    toastr add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+import { injectStyle } from "react-toastify/dist/inject-style";
 import "./App.css";
 import AppRoute from "./AppRoute";
 import Header from "./components/Header";
 
+if (typeof window !== "undefined") {
+  injectStyle();
+}
+
 function App() {
   return (
     <div>
+      <ToastContainer />
       <Header />
       <AppRoute />
     </div>
